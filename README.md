d677a865f3f6af165e88e5abe35ea0412050de19
rafaelsua <rafaelsua@gmail.com>
2020-02-11 15:37:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# litstarterkit-step3-skeleton3-44hofz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/litstarterkit-step3-skeleton3-44hofz)
---
+++ index.js
@@ -0,0 +1,39 @@
+// Import lit-html functions
+import {html, render} from 'lit-html';
+
+const items = [['A', 'Aceyalone', 'Aesop Rock', 'Angel Haze', 'Atmosphere'],['B','Babbletron','Blackalicious','Bike For Three','Black Sheep','Blueprint'],['C','Cecil Otter','Chali 2na','Chance The Rapper','Common Market','Cool Calm Pete'],['D','Danger Doom','Darc Mind','Dem Atlas','Dessa']]
+
+
+const myTemplate = ()=> html`
+  <div class="sticky-slide">
+  <dl>
+         <dt>A</dt>
+         <dd>Aceyalone</dd>
+         <dd>Aesop Rock</dd>
+         <dd>Angel Haze</dd>
+         <dd>Atmosphere</dd>
+         <dt>B</dt>
+         <dd>Babbletron</dd>
+         <dd>Blackalicious</dd>
+         <dd>Bike For Three</dd>
+         <dd>Black Sheep</dd>
+         <dd>Blueprint</dd>
+         <dt>C</dt>
+         <dd>Cecil Otter</dd>
+         <dd>Chali 2na</dd>
+         <dd>Chance The Rapper</dd>
+         <dd>Common Market</dd>
+         <dd>Cool Calm Pete</dd>
+         <dt>D</dt>
+         <dd>Danger Doom</dd>
+         <dd>Darc Mind</dd>
+         <dd>Dem Atlas</dd>
+         <dd>Dessa</dd>
+  </dl>
+</div>
+`;
+
+
+render(myTemplate(), document.body.querySelector('section')
+);
+
